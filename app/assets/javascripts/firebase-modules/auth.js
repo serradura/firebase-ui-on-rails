4abc6c9b97433ff70353f9edff8dbe8915112478
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2016-11-07 12:11:50 -0200

    Hide unused auth providers.

--- app/assets/javascripts/firebase-modules/auth.js
+++ app/assets/javascripts/firebase-modules/auth.js
@@ -41,8 +41,8 @@ $(document).on('turbolinks:load', function() {
             // 'user_friends'
           ]
         },
-        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID
       ],
       // Terms of service url.
